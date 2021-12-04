dfcb98b65eca52e03177ba163938d229b44cb5d7
Rubén Villar Grela <ruben@rubenvillar.net>
2021-12-04 23:38:07 +0100

    add touch event

--- script.js
+++ script.js
@@ -1,40 +1,48 @@
 const items = [
-  { file: 'cris', name: 'Cris'},
-  { file: 'ru', name: 'Ru'},
-  { file: 'felix', name: 'Felix'},
-  { file: 'luisi', name: 'Luisi'},
-  { file: 'lauri', name: 'Lauri'},
-  { file: 'mari', name: 'Mari'},
-]
+  { file: "cris", name: "Cris" },
+  { file: "ru", name: "Ru" },
+  { file: "felix", name: "Felix" },
+  { file: "luisi", name: "Luisi" },
+  { file: "lauri", name: "Lauri" },
+  { file: "mari", name: "Mari" },
+];
+let isSelected = false;
 
 const shuffledItems = items.sort((a, b) => 0.5 - Math.random());
 let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`);
-window.addEventListener('resize', () => {
+document.documentElement.style.setProperty("--vh", `${vh}px`);
+window.addEventListener("resize", () => {
   let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
 });
 
-const imageElements = document.querySelectorAll('.image');
+const onImageSelected = (idx, file) => {
+  if (isSelected) {
+    return;
+  }
+  isSelected = true;
+  imageElements.forEach((image, jdx) => {
+    if (jdx !== idx) {
+      image.classList.add("inactive");
+    } else {
+      image.classList.add("selected");
+    }
+  });
+  const audioObj = new Audio(`./audio/${file}.mp3`);
+  audioObj.addEventListener("ended", () => {
+    isSelected = false;
+    imageElements.forEach((image) => {
+      image.classList.remove("inactive");
+      image.classList.remove("selected");
+    });
+  });
+  audioObj.play();
+};
+
+const imageElements = document.querySelectorAll(".image");
 imageElements.forEach((img, idx) => {
-  const {file} = shuffledItems[idx];
+  const { file } = shuffledItems[idx];
   img.style.backgroundImage = `url('./img/${file}.png')`;
-  img.addEventListener('click', e => {
-    imageElements.forEach((image, jdx) => {
-      if(jdx !== idx) {
-        image.classList.add('inactive')
-      } else {
-        image.classList.add('selected')
-      }
-    })
-    const audioObj = new Audio(`./audio/${file}.mp3`);
-    audioObj.addEventListener('ended', e => {
-      imageElements.forEach((image) => {
-        image.classList.remove('inactive')
-        image.classList.remove('selected')
-      })
-    })
-    audioObj.play();
-    
-  })
-})
+  img.addEventListener("click", () => onImageSelected(idx, file));
+  img.addEventListener("touchstart", () => onImageSelected(idx, file));
+});
