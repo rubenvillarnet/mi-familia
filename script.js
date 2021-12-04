c41f3e7b837f2449120f0ca18297162ef69edace
Rubén Villar Grela <ruben@rubenvillar.net>
2021-12-05 00:08:47 +0100

    change routes

--- script.js
+++ script.js
@@ -50,7 +50,7 @@ imageElements.forEach((img, idx) => {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("./serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err))
   })
